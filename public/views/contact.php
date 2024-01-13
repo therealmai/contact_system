a26bc22a20252d33d8feca7e2615e41ac9bf74ac
Jomar Leaño <jomar35leano@gmail.com>
2024-01-13 18:56:26 +0800

    rest day

--- public/views/contact.php
+++ public/views/contact.php
@@ -164,7 +164,7 @@ $contacts = $result->fetch_all(MYSQLI_ASSOC);
                             </div>
                         </div>
                     </div>
-// comment another rest
+// comment another rest dau
                     <!-- Delete Contact Modal -->
                     <div class="modal fade" id="deleteContactModal<?php echo $contact['id']; ?>" tabindex="-1" role="dialog"
                         aria-labelledby="deleteContactModalLabel" aria-hidden="true">
